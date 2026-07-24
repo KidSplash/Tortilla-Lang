980d78fb7f35b2477d1cd614c9128b7abfabcd97
KidSplash <kidsplash00@gmail.com>
2026-07-24 14:39:10 -0500

    Updated Lexer.

--- src/cpp/Main.cpp
+++ src/cpp/Main.cpp
@@ -10,22 +10,30 @@
 
 int main() {
     std::string code = R"(
+#Comment Test
 int a = 0
-int b = a + 5
-a += b * -10
-char c = "h"
-bool question = true
-question = not question
-thisisafloat -= 17.1
+int b = 15.5
+float c =12
+float d= 4.1
+char word = "a"
+bool notABool = 0
+bool aBool = false
+a = null
+b += aBool
+word = word + word
+##Heres another
+comment##
+aBool = not aBool
+c = b / a
 )";
 
     std::vector<Token> tokens = tokenize(code);
     //decodeTokens(tokens);
     //std::cout << "\n\n";
-    PrgmNode ast(0, {});
-    ast = parse(tokens);
+    //PrgmNode ast(0, {});
+    //ast = parse(tokens);
     //decodeAST(std::move(ast));
-    std::unordered_map<std::string, Variable> vars = nameCheckAST(std::move(ast));
+    //std::unordered_map<std::string, Variable> vars = nameCheckAST(std::move(ast));
     //for (const auto& [key, value] : vars) {
         //std::cout << key << "\n";
     //}
